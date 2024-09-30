e68340657d65aaa1f11f94a469b61fa4eacba0b3
James Beeber <jhbeeber@gmail.com>
2024-09-30 19:38:48 +0000

    Added dependency for environment file loader

--- myHealthArc/Package.swift
+++ myHealthArc/Package.swift
@@ -17,6 +17,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/leaf.git", from: "4.3.0"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        .package(url: "https://github.com/swiftpackages/DotEnv.git", from: "3.0.0"),
     ],
     targets: [
         .executableTarget(
@@ -28,6 +29,7 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
+                .product(name: "DotEnv", package: "DotEnv"),
             ],
             swiftSettings: swiftSettings
         ),

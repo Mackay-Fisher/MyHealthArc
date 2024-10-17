5724ae5a8fc5aee526980a0bf1f017dc7691c406
cevancura <122992309+cevancura@users.noreply.github.com>
2024-10-17 16:07:17 -0500

    Upload new-frontend created with XCode 15.2

---
+++ myHealthArc-new-frontend/myHealthArc-new-frontend/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myHealthArc-new-frontend
+//
+//  Created by Vancura, Christiana Elaine on 10/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myHealthArc-new-frontend/myHealthArc-new-frontend/myHealthArc_new_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  myHealthArc_new_frontendApp.swift
+//  myHealthArc-new-frontend
+//
+//  Created by Vancura, Christiana Elaine on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct myHealthArc_new_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

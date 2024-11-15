a9b8044a04e48d1680279450a96f1477a7301a1b
rheaphatak <100486743+rheaphatak@users.noreply.github.com>
2024-11-15 16:47:42 -0600

    add dietary options

--- myHealthArc-new-frontend/myHealthArc-new-frontend/ChatbotView.swift
+++ myHealthArc-new-frontend/myHealthArc-new-frontend/ChatbotView.swift
@@ -13,24 +13,69 @@ struct ChatbotView: View {
    
     var body: some View {
         VStack {
-            MessagesListView(messages: viewModel.messages)
-            HStack {
-                TextField("Enter your message", text: $userMessage)
-                    .padding(.horizontal)
-                    .padding(.vertical, 10)
-                    .cornerRadius(12)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(Color.gray, lineWidth: 0.5)
-                    )
-                    .padding()
-                
-                Button(action: sendMessage) {
-                    Text("Send")
+            Button(action: {
+            }) {
+                HStack {
+                    Image(systemName: "lightbulb.fill")
+                        .frame(width: 28, height: 28)
+                        .foregroundColor(.white)
+                    Text("Recipe Assistant")
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
                 }
-                .padding(.trailing)
+                .padding(10)
+                .background(Color.blue)
+                .cornerRadius(12)
+                .shadow(radius: 5)
+                .padding(.leading, 20)
+                .padding(.top, 20)
+                Spacer()
             }
-            .padding(.bottom)
+            VStack {
+                MessagesListView(messages: viewModel.messages)
+                
+                var dietaryOptions = ["Vegetarian", "Vegan", "Gluten-Free"]
+                var selectedDietaryOption = ""
+                HStack {
+                    ForEach(dietaryOptions, id: \.self) { option in
+                        Button(action: {
+                            viewModel.sendUserMessage(option)
+                        }) {
+                            Text(option)
+                                .font(.body)
+                                .padding(8)
+                                .cornerRadius(10)
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        .padding(2)
+                        .cornerRadius(12)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(Color.gray, lineWidth: 0.5)
+                        )
+                    }
+                }
+                .padding()
+                
+                HStack {
+                    TextField("Enter your message", text: $userMessage)
+                        .padding(.horizontal)
+                        .padding(.vertical, 10)
+                        .cornerRadius(12)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(Color.gray, lineWidth: 0.5)
+                        )
+                        .padding()
+                    
+                    Button(action: sendMessage) {
+                        Text("Send")
+                    }
+                    .padding(.trailing)
+                }
+                .padding(.bottom)
+            }
         }
 
     }
